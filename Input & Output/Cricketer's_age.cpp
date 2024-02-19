fa161178a8e4424d6faa5bbf326f63a86b084332
Kanish <kanish2233@gmail.com>
2024-02-19 22:54:59 +0530

    Create Cricketer's_age.cpp

---
+++ Input & Output/Cricketer's_age.cpp
@@ -0,0 +1,25 @@
+// You are collecting the age of each cricketers in the team. At a time, you can get only one cricketer age. So, create a program, which will get that one cricketer's age and print the age to verify.
+
+// Input Format:
+// Input value consists of an Integer.
+
+// Output Format:
+// Output value consists of an Integer.
+
+// Sample Input:
+// 29
+
+// Sample Output:
+// The cricketer age is 29
+
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int age;
+    cin>>age;
+    cout<<"The cricketer age is "<<age;
+    return 0;
+}
