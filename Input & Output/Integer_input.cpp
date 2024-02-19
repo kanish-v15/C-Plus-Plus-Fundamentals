afb255deeae1d36e3d1d3c751c886e8f654f8c66
Kanish <kanish2233@gmail.com>
2024-02-19 22:48:55 +0530

    Create Integer_input.cpp

---
+++ Input & Output/Integer_input.cpp
@@ -0,0 +1,21 @@
+// Write a program to get an integer from the user and display it.
+
+// INPUT & OUTPUT FORMAT: 
+
+// Input consist of 1 integer. Output consist of 1 integer.
+
+// Sample Input:
+
+// 56
+// Sample Output:
+// 56
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int input_num;
+    cin>>input_num;
+    cout<<input_num;
+    return 0;
+}
