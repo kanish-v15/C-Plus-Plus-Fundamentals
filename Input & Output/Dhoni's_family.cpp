cf746c7e7a8cbd2aa0a109e1e04ef11ac7b0fdaa
Kanish <kanish2233@gmail.com>
2024-02-19 23:00:34 +0530

    Create Dhoni's_family.cpp

---
+++ Input & Output/Dhoni's_family.cpp
@@ -0,0 +1,30 @@
+// Dhoni's family is from the community of the Rajputs and a simple middle-class family. Dhoni's father Pan Singh Dhoni worked for MECON, a public-sector engineering firm and his mother Devki Devi, a housewife. He has an elder brother Narendra Singh Dhoni who is a politician and a sister Jayanti Gupta who is a teacher.
+
+// Write a program to display Dhoni's family details, in the following format:
+// M. S. Dhoni
+// Father: Mr. Pan Singh
+// Mother: Mrs. Devki Devi
+// Sibings: Narendra Singh and Jayanti Gupta
+
+// Input Format:
+// There is no Input
+
+// Output Format:
+// Print the dhoni family details as given above.
+
+// Refer sample input and output for formatting specifications.
+
+// Sample Output:
+// M. S. Dhoni
+// Father: Mr. Pan Singh
+// Mother: Mrs. Devki Devi
+// Sibings: Narendra Singh and Jayanti Gupta
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"M.S, MSD, Mahi, Thala, Ranchi Rocker, Captain Cool";
+    return 0;
+}
