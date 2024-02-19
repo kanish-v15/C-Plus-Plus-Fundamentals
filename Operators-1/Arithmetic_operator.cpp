6fb9f16847f13bea33125c1571b14175ed6638c2
Kanish <kanish2233@gmail.com>
2024-02-19 23:03:48 +0530

    Create Arithmetic_operator.cpp

---
+++ Operators-1/Arithmetic_operator.cpp
@@ -0,0 +1,39 @@
+// Write a C program to do an Arithmetic Operators.
+
+// Input Format: 
+
+// Input consists of two integers 
+
+// Output Format:
+
+// Output consist of integers
+
+// Sample Input :
+
+// 7
+
+// 5
+
+// Sample Output :
+
+// 7 + 5 = 12
+// 7 - 5 = 2
+// 7 * 5 = 35
+// 7 / 5 = 1
+// 7 % 5 = 2
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num_one, num_two;
+    cin>>num_one;
+    cin>>num_two;
+    cout<<num_one<<" + "<<num_two<<" = "<<num_one+num_two<<"\n";
+    cout<<num_one<<" - "<<num_two<<" = "<<num_one-num_two<<"\n";
+    cout<<num_one<<" * "<<num_two<<" = "<<num_one*num_two<<"\n";
+    cout<<num_one<<" / "<<num_two<<" = "<<num_one/num_two<<"\n";
+    cout<<num_one<<" % "<<num_two<<" = "<<num_one%num_two;
+    return 0;
+}
