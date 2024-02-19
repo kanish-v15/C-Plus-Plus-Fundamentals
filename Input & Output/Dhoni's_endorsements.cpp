72d2417e6ff6fe597f4d6441dba31a99170fbce1
Kanish <kanish2233@gmail.com>
2024-02-19 23:01:48 +0530

    Create Dhoni's_endorsements.cpp

---
+++ Input & Output/Dhoni's_endorsements.cpp
@@ -0,0 +1,27 @@
+// MS Dhoni is the highest-paid batsman on the endorsement circuit and he was ranked 23rd by Forbes amoung the world's highest-paid athletes last year - the only cricketer on the list - with $27 million in earning from endorsements.
+
+// Currently Dhoni has 20 endorsements only one less than Shahrukh Khan. It includes Pepsica, AIRCEL Communications, Titan, TVS Motors, Boost, Reebok, Videocon, GE Money, NDTV, etc.,.
+
+// Write a program to print th list of any 10 endorsements that Dhoni owns.
+
+// Refer the Sample Output.
+
+// Sample Output:
+// Pepsico
+// AIRCEL
+// Communications
+// Titan
+// TVS
+// Motors
+// Boost
+// Reebok
+// Videocon
+// GE Money
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Pepsico\nAIRCEL\nCommunications\nTitan\nTVS\nMotors\nBoost\nReebok\nVideocon\nGE Money";
+    return 0;
+}
