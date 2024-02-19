1de44f5b4e2ab62d3d973a8c03926f3a72a72d51
Kanish <kanish2233@gmail.com>
2024-02-19 22:52:58 +0530

    Create Welcome.cpp

---
+++ Input & Output/Welcome.cpp
@@ -0,0 +1,18 @@
+// WELCOME TEAM
+
+// You reside in a desert place, One day a new group of peoples came to visit the desert place, infront of your eyes. Two peoples comes out of the group. 
+
+// They were so friendly and you just want to Welcome them to the place. Write a program to welcome those two peoples to the desert.
+
+// SAMPLE OUTPUT:
+
+// Hello People! Welcome to our desert.
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hello People! Welcome to our desert.";
+    return 0;
+}
