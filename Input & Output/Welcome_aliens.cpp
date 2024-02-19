7809b96ea9d8f8c7685f75952587e4af4045b87b
Kanish <kanish2233@gmail.com>
2024-02-19 22:56:40 +0530

    Create Welcome_aliens.cpp

---
+++ Input & Output/Welcome_aliens.cpp
@@ -0,0 +1,18 @@
+// WELCOME ALIENS
+
+// You reside in a desert place, One day a space shuttle lands infront of your eyes. Two Aliens comes out of the space shuttle. 
+
+// They were so friendly and you just want to Welcome them to our planet. Write a program to welcome those two aliens to our planet EARTH.
+
+// SAMPLE OUTPUT:
+
+// Hello Aliens ! Welcome to our planet Earth.
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hello Aliens ! Welcome to our planet Earth.";
+    return 0;
+}
