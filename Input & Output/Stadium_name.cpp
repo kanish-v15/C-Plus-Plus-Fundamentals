c059b42d0c95a30a28a30168a1b3c7df53b0a2b0
Kanish <kanish2233@gmail.com>
2024-02-19 22:53:57 +0530

    Create Stadium_name.cpp

---
+++ Input & Output/Stadium_name.cpp
@@ -0,0 +1,24 @@
+// MECON LIMITED was a public sector undertaking firm under the ministry of steel where Dhoni's father had worked for. It was the stadium where MS Dhoni grew up playing.
+
+// The Chairman of MECON decided to renovate and improvise the facilities in the stadium and to place a NEON display board at the entry of the stadium that carried the Stadium's name as "MECON Stadium".
+
+// Write a program to display the Stadium name in different lines.
+
+// Input Format:
+// There is no input
+
+// Output Format:
+// Print the name "MECON Stadium" in different lines.
+
+// SAMPLE OUTPUT:
+// MECON
+// Stadium
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"MECON\nStadium";
+    return 0;
+}
