c83da794d089535ed21e12cc87830f67df7749b9
Kanish <kanish2233@gmail.com>
2024-02-19 22:57:30 +0530

    Create Dhoni's_nickname.cpp

---
+++ Input & Output/Dhoni's_nickname.cpp
@@ -0,0 +1,21 @@
+// MS Dhoni is one of the cricket's most charismatic personalities today. He has been a true inspirational figure and has provided for some great cricketing moments. He has a huge fan base across borders irrespective of age, nationality, nor languages spoken. India, being the a diverse country of ethnic culture, so does the Indian Captian who now has nick name in diversified ways like M.S, MSD, Mahi, Thala, Ranchi Rocker, Captian Cool, etc.,. Write a program to  print the various nick names of Dhoni separated by comma.
+
+// Input Format:
+// There is no input.
+
+// Output Format:
+// Print the nick names of Dhoni separated by comma.
+
+// Refer the sample input and output for formatting specifications.
+
+// Sample Output:
+// M.S, MSD, Mahi, Thala, Ranchi Rocker, Captain Cool
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"M.S, MSD, Mahi, Thala, Ranchi Rocker, Captain Cool";
+    return 0;
+}
